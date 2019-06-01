d6ef9661c20e6005b8e178d058edfa97e7256956
Sachin Handiekar <sachin.handiekar@gmail.com>
2019-06-01 20:19:56 +0100

    Updated posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,11 @@
 'use strict';
+import moment from 'moment'
 
 exports.createPages = require('./gatsby/create-pages');
 exports.onCreateNode = require('./gatsby/on-create-node');
+
+exports.rewritePath = (parsedFilePath, metadata) => {
+  if (parsedFilePath.ext === "md") {
+    return `/${moment(metadata.createdAt).format('YYYY')}/${moment(metadata.createdAt).format('MM')}/${parsedFilePath.name}/`
+  }
+}
