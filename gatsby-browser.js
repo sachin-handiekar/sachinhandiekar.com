6095d28bf0dc15300329d588cbf837c1d93d8cdb
Sachin Handiekar <sachin.handiekar@gmail.com>
2019-11-24 21:16:16 +0000

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,8 @@
 'use strict';
 
-require('./src/assets/scss/init.scss');
+require("./src/assets/scss/init.scss");
 require("prismjs/themes/prism-tomorrow.css")
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
-require('./src/assets/scss/prism-custom.scss');
+require("./src/assets/scss/prism-custom.scss");
 
 exports.onClientEntry = () => {};
-
